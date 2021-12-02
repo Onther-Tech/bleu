5a8ab4b378014b242a590f8d8eeb75e099c8629d
Harry Oh <harry@5004.pe.kr>
2021-12-03 08:40:51 +0900

    Change configurations

--- bleu-app/src/utils/consts.ts
+++ bleu-app/src/utils/consts.ts
@@ -1,3 +1,3 @@
-export const L1ExplorerEndpoint = 'https://etherscan.io';
-export const BleuServerEndpoint = '';
-export const L2JsonRpcEndpoint = '';
+export const L1ExplorerEndpoint = 'https://rinkeby.etherscan.io';
+export const BleuServerEndpoint = 'http://localhost:8888';
+export const L2JsonRpcEndpoint = 'https://testnet2.optimism.tokamak.network/';
