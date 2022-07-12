c761202750f083ae55d43259a2a81b1427386389
Harry Oh <harry@5004.pe.kr>
2022-07-12 11:23:07 +0900

    Default endpoint is changed to testnet3(darius)

--- bleu-app/src/utils/consts.ts
+++ bleu-app/src/utils/consts.ts
@@ -1,6 +1,5 @@
 export const L1ExplorerEndpoint = 'https://rinkeby.etherscan.io';
 export const BleuServerEndpoint = 'http://localhost:8888';
-export const L2JsonRpcEndpoint = 'https://testnet2.optimism.tokamak.network/';
+export const L2JsonRpcEndpoint = 'https://testnet3.optimism.tokamak.network/';
 
 export const MainPageAutoRefresh = false;
-
