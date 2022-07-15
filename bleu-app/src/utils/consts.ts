bdbdf606e38a1b4fb3ffd85e28de0349a2b75ee2
Harry Oh <harry@5004.pe.kr>
2022-07-15 16:31:42 +0900

    Change BleuServerEndpoint for frontend

--- bleu-app/src/utils/consts.ts
+++ bleu-app/src/utils/consts.ts
@@ -1,5 +1,5 @@
 export const L1ExplorerEndpoint = 'https://rinkeby.etherscan.io';
-export const BleuServerEndpoint = 'http://localhost:8888';
+export const BleuServerEndpoint = 'http://testnet3.explorer.optimism.tokamak.network:8888';
 export const L2JsonRpcEndpoint = 'https://testnet3.optimism.tokamak.network/';
 
 export const MainPageAutoRefresh = true;
